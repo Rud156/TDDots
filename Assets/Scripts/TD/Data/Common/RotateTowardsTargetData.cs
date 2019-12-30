7568fd33d7de94ff4e6c23cab391eea74bd7d32c
Rud156 <rudrangshu_1997@yahoo.com>
2019-12-30 15:52:20 -0700

    Updated Basic Enemy Movement

---
+++ Assets/Scripts/TD/Data/Common/RotateTowardsTargetData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace TD.Data.Common
+{
+    [GenerateAuthoringComponent]
+    public struct RotateTowardsTargetData : IComponentData
+    {
+        public float rotationSpeed;
+
+        [Header("Internal Data")] public float3 _lastPosition;
+    }
+}
---
+++ Assets/Scripts/TD/Systems/Common/RotateTowardsTargetSystem.cs
@@ -0,0 +1,47 @@
+﻿using TD.Data.Common;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace TD.Systems.Common
+{
+    public class RotateTowardsTargetSystem : JobComponentSystem
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+
+            Entities.ForEach((ref RotateTowardsTargetData rotateTowardsTarget, in Translation translation) =>
+            {
+                rotateTowardsTarget._lastPosition = translation.Value;
+            }).Run();
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            float deltaTime = Time.DeltaTime;
+
+            JobHandle jobHandle = Entities.ForEach((ref RotateTowardsTargetData rotateTowardsTarget,
+                ref Rotation rotation,
+                in Translation translation) =>
+            {
+                float3 previousPosition = rotateTowardsTarget._lastPosition;
+                float3 currentPosition = translation.Value;
+
+                float3 directionVector = (previousPosition - currentPosition);
+                quaternion rotationValue = quaternion.LookRotation(directionVector, math.up());
+
+                rotation.Value = math.slerp(
+                    rotation.Value,
+                    rotationValue,
+                    deltaTime * rotateTowardsTarget.rotationSpeed
+                );
+
+                rotateTowardsTarget._lastPosition = currentPosition;
+            }).Schedule(inputDeps);
+
+            return jobHandle;
+        }
+    }
+}
