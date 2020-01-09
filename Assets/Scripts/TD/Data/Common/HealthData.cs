b9d8446dfdb9656b5f82f2438b6c25350e776614
Rud156 <rudrangshu_1997@yahoo.com>
2020-01-08 19:03:22 -0700

    Added Projectiles

---
+++ Assets/Scripts/TD/Data/Common/HealthData.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace TD.Data.Common
+{
+    [GenerateAuthoringComponent]
+    public struct HealthData : IComponentData
+    {
+        public float maxHealth;
+
+        [Header("Internal Data")] public float currentHelth;
+    }
+}
