3c65019bc4ddc581f74b97a4794ed2a9c229d3cf
Rud156 <rudrangshu_1997@yahoo.com>
2020-01-29 01:35:18 -0700

    Added Player Movement

--- Assets/Scripts/TD/Data/Player/PlayerInputData.cs
+++ Assets/Scripts/TD/Data/Player/PlayerInputData.cs
@@ -7,6 +7,9 @@ namespace TD.Data.Player
     [GenerateAuthoringComponent]
     public struct PlayerInputData : IComponentData
     {
+        public float movementSpeed;
+        public float rotationSpeed;
+
         [Header("Internal Data")] public float2 _movementData;
         public bool _lastFrameShot;
     }
